14602cce68980135fc61b4ece308bdb7bd155921
Amir <herus2006cripto@gmail.com>
2024-07-07 16:20:40 +0330

    initiate commit

---
+++ README.md
@@ -0,0 +1,3 @@
+this is initial commit - you are reading a doctor appointment application code base.
+
+I appreciate you taking the time 
---
+++ components/template/homePage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const homePage = () => {
+  return (
+    <div>homePage</div>
+  )
+}
+
+export default homePage
